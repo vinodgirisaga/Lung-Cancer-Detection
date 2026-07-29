481994a76e1f06e3d3d67389ff006bf253926fd4
vinodgirisaga <vinodgirisagar7@gmail.com>
2026-07-29 07:31:34 -0700

    Add files via upload
    # Lung Cancer Detection
    
    ## Description
    This is a Machine Learning project to predict lung cancer using Python.
    
    ## Technologies Used
    - Python
    - Pandas
    - NumPy
    - Scikit-learn
    - Matplotlib
    
    ## Features
    - Data preprocessing
    - Lung cancer prediction
    - Machine Learning model
    
    ## Author
    Vinod Girisagar

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# Lung-Cancer-Detection
-Machine Learning project for Lung Cancer Detection using Python and Computer based analusis and  used python with ML libraries like numpy, pandas, and matplotlib etc. And analyze patient data and detect lung cancer prediction. And display "STREMLIT" web app and This project is helpfully use in early detection and awerness.
+# Lung Cancer Detection using Machine Learning
+
+- If lung cancer is found at an earlier stage, when it is small and before it has spread, it is more likely to be treated successfully. Lung cancer screening is recommended for certain people who smoke or used to smoke, but who don't have any signs or symptoms. If a person has lung cancer but doesn't have any symptoms, this usually means there's a chance to detect the disease early.
+
+- Lung Cancer Detection using Machine Learning and Computer Vision is a project that aims to develop a system that can automatically detect the presence of lung cancer in medical images of the lungs, such as X-rays or CT scans, using advanced algorithms and techniques from the fields of machine learning and computer vision. The system will be trained on a large dataset of annotated medical images and will use various image processing techniques to extract relevant features and patterns from the images. 
+
+- The output of the system will be a prediction of whether or not lung cancer is present in the image, which can be used to assist medical professionals in making accurate and timely diagnoses. The project involves the use of state-of-the-art machine learning algorithms, such as convolutional neural networks (CNNs), and advanced image processing techniques, such as edge detection, segmentation, and feature extraction.
+
+- Check out the live Deployment at - [Streamlit Deployment](https://vedantkadam-lung-cancer-streamlit-app-le67uq.streamlit.app/) 
+## How to Run this Project
+
+## Tech Stacks Used
+
+- Python
+- Streamlit API
+- Tensorflow , Keras API
+- Numpy , Pandas , Seaborn
+
+## How to run this Project
+```
+git clone URL
+```
+```
+cd LungCancerDetection
+```
+
+- Install modules if not available using following commands
+```
+pip install -r requirements.txt
+```
+
+- Run the app.py file
+```
+streamlit run app.py
+```
+
+- Note- Sometimes models as well as csv file might not be detected due to environment related issue it is recommended that you install Anaconda and create a seperate environment by the name 'tf' install required modules.
+Then run the following commands-
+```
+activate conda tf
+cd LungCancerDetection
+streamlit run app.py
+
+```
+
+
+## Architecture
+<p align="center">
+  <a href="/">
+    <img src="https://user-images.githubusercontent.com/83024561/219716865-f5bb045e-781d-4281-ae13-b86fcaab911a.png"
+         alt="csi_logo" width="800" height="400">
+  </a>
+</p>
+
+## Project Demo
+
+https://user-images.githubusercontent.com/83024561/217852039-1365ed15-9963-4cac-b19b-fe7be9ff5794.mp4
+
+
+
+
+
+
+
+
+
+
+
---
+++ app.py
@@ -0,0 +1,345 @@
+import pickle
+import streamlit as st
+import numpy as np
+import pyparsing
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+from PIL import Image 
+import tensorflow
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+from tempfile import NamedTemporaryFile
+from tensorflow.keras.preprocessing import image 
+from streamlit_option_menu import option_menu
+st.set_page_config(page_title='Lung Cancer Detection')
+#Loading models
+cancer_model = pickle.load(open('models/final_model.sav', 'rb'))
+
+
+with st.sidebar:
+    selection = option_menu('Lung Cancer Detection System',
+    ['Introduction',
+    'About the Dataset',
+    'Lung Cancer Prediction',],
+    icons = ['activity','heart','person', 'heart'],
+    default_index = 0)
+    
+
+#Introduction page
+if (selection == 'Introduction'):
+
+    from PIL import Image
+
+    gg = Image.open("images/lung-cancer.jpg")
+
+    st.image(gg, caption='Introduction to Lung Cancer',width=600)
+    #page title
+    st.title('How common is lung cancer?')
+
+    st.write("Lung cancer (both small cell and non-small cell) is the second most common cancer in both men and women in the United States (not counting skin cancer). In men, prostate cancer is more common, while in women breast cancer is more common.")
+    st.markdown(
+    """
+    The American Cancer Society’s estimates for lung cancer in the US for 2023 are:
+    - About 238,340 new cases of lung cancer (117,550 in men and 120,790 in women)
+    - About 127,070 deaths from lung cancer (67,160 in men and 59,910 in women)
+
+    """
+    )
+
+    st.write("")
+    st.title("Is Smoking the only cause ?")
+    mawen = Image.open("images/menwa.png")
+
+    st.image(mawen, caption='Smoking is not the major cause',width=650)
+    #page title
+    
+    st.write("The association between air pollution and lung cancer has been well established for decades. The International Agency for Research on Cancer (IARC), the specialised cancer agency of the World Health Organization, classified outdoor air pollution as carcinogenic to humans in 2013, citing an increased risk of lung cancer from greater exposure to particulate matter and air pollution.")
+
+
+
+
+    st.markdown(
+    """
+    The following list won't indent no matter what I try:
+    - A 2012 study by Mumbai’s Tata Memorial Hospital found that 52.1 per cent of lung cancer patients had no history of smoking. 
+    - The study contrasted this with a Singapore study that put the number of non-smoking lung cancer patients at 32.5 per cent, and another in the US that found the number to be about 10 per cent.
+    - The Tata Memorial study found that 88 per cent of female lung cancer patients were non-smokers, compared with 41.8 per cent of males. It concluded that in the case of non-smokers, environmental and genetic factors were implicated.
+    """
+    )
+
+    st.title("Not just a Delhi phenomenon ")
+    stove = Image.open("images/stove.png")
+
+    st.image(stove, caption='Smoking is not the major cause',width=650)
+    #page title
+    st.markdown(
+    """
+    The following list won't indent no matter what I try:
+    - In January 2017, researchers at AIIMS, Bhubaneswar, published a demographic profile of lung cancer in eastern India, which found that 48 per cent of patients had not been exposed to active or passive smoking
+    - 89 per cent of women patients had never smoked, while the figure for men was 28 per cent.
+    - From available research, very little is understood about lung cancer among non-smokers in India. “We need more robust data to identify how strong is the risk and link,” Guleria of AIIMS says.
+    """
+    )
+
+if (selection == 'About the Dataset'):
+    tab1, tab2, tab3 , tab4 = st.tabs(["Dataset analysis", "Training Data", "Test Data","Algorithms Used"])
+
+    with tab1:
+        
+        st.header("Lung Cancer Dataset")
+        data=pd.read_csv("datasets/data.csv")
+        st.write(data.head(10))
+        code = '''
+        Index(['Age', 'Gender', 'Air Pollution', 'Alcohol use', 'Dust Allergy',
+       'OccuPational Hazards', 'Genetic Risk', 'chronic Lung Disease',
+       'Balanced Diet', 'Obesity', 'Smoking', 'Passive Smoker', 'Chest Pain',
+       'Coughing of Blood', 'Fatigue', 'Weight Loss', 'Shortness of Breath',
+       'Wheezing', 'Swallowing Difficulty', 'Clubbing of Finger Nails',
+       'Frequent Cold', 'Dry Cough', 'Snoring', 'Level'],
+      dtype='object')'''
+        st.code(code, language='python')
+
+        
+        st.header("Pearson Correlation Matrix")
+        coors = Image.open("images/coors.png")
+
+        st.image(coors, caption='Pearson Correlation Matrix',width=800)
+        st.write("From the above co-relation matrix we did apply a function which picks out values based on their high correlation with a particular attribute which could be dropped to improve Machine Learning Models Performance")
+        st.markdown( """
+            
+            - The Following Attributed are as follows :-
+            """)
+        code = '''{'Chest Pain',
+ 'Coughing of Blood',
+ 'Dust Allergy',
+ 'Genetic Risk',
+ 'OccuPational Hazards',
+ 'chronic Lung Disease'}'''
+        st.code(code, language='python')
+
+    with tab2:
+        st.header("Lung Cancer Training Dataset")
+
+        st.subheader("X_Train Data")
+        data=pd.read_csv("datasets/train.csv", index_col=0)
+        st.write(data)
+        code = ''' Index(['Age', 'Gender', 'Air Pollution', 'Alcohol use', 'Balanced Diet',
+       'Obesity', 'Smoking', 'Passive Smoker', 'Fatigue', 'Weight Loss',
+       'Shortness of Breath', 'Wheezing', 'Swallowing Difficulty',
+       'Clubbing of Finger Nails', 'Frequent Cold', 'Dry Cough', 'Snoring'],
+        dtype='object')'''
+        st.code(code, language='python')
+        data=pd.read_csv("datasets/trainy.csv", index_col=0)
+        st.subheader("Y_Train Data")
+        st.dataframe(data, use_container_width=True)
+
+
+       
+
+    with tab3:
+        st.header("Lung Cancer Training Dataset")
+
+        st.subheader("X_Test Data")
+        data=pd.read_csv("datasets/testx.csv", index_col=0)
+        st.write(data)
+        code = ''' Index(['Age', 'Gender', 'Air Pollution', 'Alcohol use', 'Balanced Diet',
+       'Obesity', 'Smoking', 'Passive Smoker', 'Fatigue', 'Weight Loss',
+       'Shortness of Breath', 'Wheezing', 'Swallowing Difficulty',
+       'Clubbing of Finger Nails', 'Frequent Cold', 'Dry Cough', 'Snoring'],
+        dtype='object')'''
+        st.code(code, language='python')
+        data=pd.read_csv("datasets/testy.csv", index_col=0)
+        st.subheader("Y_Test Data")
+        st.dataframe(data, use_container_width=True)
+        
+    with tab4:
+        st.header("List of Algorithms Used")
+        algo = Image.open("images/algo.png")
+
+        st.image(algo, caption='ML Algorithms',width=500)
+
+        st.write("Since this is a Mutlti-Class Classification we have used Algorithms which are maily used for Supervised Learning for the following Problem Statement ")
+
+        st.markdown(
+            """
+            Supervised Learning Algorithms:
+            - Linear Regression
+            - Support Vector Machine
+            - K-Nearest Neighbours (KNN)
+            - Decision Tree Classifier
+            """
+            )
+        
+        st.write("The accuracy of all the above algorithms is as follows:- ")
+        code = '''The accuracy of the SVM is: 95 %
+        The accuracy of the SVM is: 100 %
+        The accuracy of Decision Tree is: 100 %
+        The accuracy of KNN is: 100 %'''
+        st.code(code, language='python')
+
+        st.header("Confusion Matrix")
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            algo = Image.open("images/lg.png")
+
+            st.image(algo, caption='LG Confusion Matrix',width=350)
+
+        with col2:
+            algo = Image.open("images/svm.png")
+
+            st.image(algo, caption='SVM Confusion Matrix',width=390)
+
+
+
+        st.subheader("Plotting the Traning vs Validation (Accuracy and Loss)")
+        col1, col2 = st.columns(2)
+
+        with col1:
+            acc = Image.open("images/acc.png")
+
+            st.image(acc, caption='Traning vs Validation Accuracy',width=350)
+
+        with col2:
+            loss = Image.open("images/loss.png")
+
+            st.image(loss, caption='Traning vs Validation Loss',width=350)
+
+        st.write("As we can see from the above diagram that our Models performs well on the Training as well as Validation Data")
+
+
+        
+    
+
+# Lung Cancer disease Prediction pages
+if (selection == 'Lung Cancer Prediction'):
+    #page title
+    
+    ##this code is reduce effort of manuallu entering values
+    testx=pd.read_csv("datasets/testx.csv",index_col=0)
+    testy=pd.read_csv("datasets/testy.csv",index_col=0)
+    testx.reset_index(drop=True, inplace=True)
+    testy.reset_index(drop=True, inplace=True)
+    
+    concate_data = pd.concat([testx,testy],axis=1)
+
+    
+    
+
+    st.title('Lung Cancer Prediction using ML')
+
+    idn = st.slider('Select any index from Testing Data', 0, 200, 25)
+    a=concate_data.iloc[idn]
+    st.write("Displaying vales of index ", idn)
+    aa=list(concate_data.iloc[idn])
+    if st.button('Show me this value'):
+        st.write(aa)
+
+    ##values will come directly from here no manual needed
+    a=concate_data.iloc[idn][0]
+    b=concate_data.iloc[idn][1]
+    c=concate_data.iloc[idn][2]
+    d=concate_data.iloc[idn][3]
+    e=concate_data.iloc[idn][4]
+    f=concate_data.iloc[idn][5]
+    g=concate_data.iloc[idn][6]
+    h=concate_data.iloc[idn][7]
+    i=concate_data.iloc[idn][8]
+    j=concate_data.iloc[idn][9]
+    k=concate_data.iloc[idn][10]
+    l=concate_data.iloc[idn][11]
+    m=concate_data.iloc[idn][12]
+    n=concate_data.iloc[idn][13]
+    o=concate_data.iloc[idn][14]
+    p=concate_data.iloc[idn][15]
+    q=concate_data.iloc[idn][16]
+
+
+
+    
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        
+        Age = st.text_input('Age', key="1",value=a)
+        
+    with col2:
+        Gender = st.text_input('Gender', key="2",value=b)
+        
+    with col3:
+        AirPollution = st.text_input('Air Pollution', key="3",value=c)
+
+    with col1:
+        Alcoholuse = st.text_input('Alcohol Use', key="4",value=d)  
+
+    with col2:
+        BalancedDiet = st.text_input('Balanced Diet', key="5",value=e)
+        
+    with col3:
+        Obesity = st.text_input('Obesity', key="6",value=f)
+        
+    with col1:
+        Smoking = st.text_input('Smoking', key="7",value=g)
+        
+    with col2:
+        PassiveSmoker = st.text_input('Passive Smoker', key="8",value=h)
+        
+    with col3:
+        Fatigue = st.text_input('Fatigue', key="9",value=i)
+        
+    with col1:
+        WeightLoss = st.text_input('Weight Loss', key="10",value=j)
+        
+    with col2:
+        ShortnessofBreath = st.text_input('Shortness of Breath', key="11",value=k)
+        
+    with col3:
+        Wheezing = st.text_input('Wheezing', key="12",value=l)
+        
+    with col1:
+        SwallowingDifficulty = st.text_input('Swallowing Difficulty', key="13",value=m)
+        
+    with col2:
+        ClubbingofFingerNails = st.text_input('Clubbing of Finger Nails', key="14",value=n)
+
+    with col3:
+        FrequentCold = st.text_input('Frequent Cold', key="15",value=o)
+        
+    with col1:
+        DryCough = st.text_input('Dry Cough', key="16",value=p)    
+     
+    with col2:
+        Snoring = st.text_input('Snoring  ', key="17",value=q)
+ 
+    # code for Prediction
+    heart_diagnosis = ''
+    
+    # creating a button for Prediction
+    
+    if st.button('Heart Disease Test Result'):
+        heart_prediction = cancer_model.predict([[Age, Gender, AirPollution, Alcoholuse, BalancedDiet, Obesity, Smoking, PassiveSmoker, Fatigue, WeightLoss,ShortnessofBreath, Wheezing, SwallowingDifficulty,ClubbingofFingerNails, FrequentCold, DryCough, Snoring]])                          
+        
+        if (heart_prediction[0] == 'High'):
+          heart_diagnosis = 'The person is having heart disease'
+          st.error(heart_diagnosis)
+
+        elif(heart_prediction[0] == 'Medium'):
+          heart_diagnosis = 'The person is chance of having heart disease'
+          st.warning(heart_diagnosis)
+        else:
+          heart_diagnosis = 'The person does not have any heart disease'
+          st.balloons()
+          st.success(heart_diagnosis)
+        
+        
+
+    expander = st.expander("Here are some more random values from Test Set")
+    
+    expander.write(concate_data.head(200))
+    
+
+    
+
+
+
